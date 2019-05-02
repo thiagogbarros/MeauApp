1f198d497ee3cda16796daf8f5fea62195210b60
Thiago <thiagobarros@cjr.org.br>
2019-05-02 12:33:05 -0300

    Criando a adotar.class e sua tela em branco

---
+++ app/src/main/java/com/example/meauapp/Adotar.java
@@ -0,0 +1,12 @@
+package com.example.meauapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Adotar extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adotar);
+    }
+}
