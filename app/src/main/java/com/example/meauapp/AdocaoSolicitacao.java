d20c28c74b751c0064553b9c80a9574f1d67cf87
Thiago <thiagobarros@cjr.org.br>
2019-05-16 20:02:46 -0300

    Criando a pagina de solicitação de adoção

---
+++ app/src/main/java/com/example/meauapp/AdocaoSolicitacao.java
@@ -0,0 +1,13 @@
+package com.example.meauapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdocaoSolicitacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adocao_solicitacao);
+    }
+}
